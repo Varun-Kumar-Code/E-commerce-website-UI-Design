6c4da7f17320f6627daead2f6762d1086122c145
Varun Kumar <156886368+Varun-Kumar-R13@users.noreply.github.com>
2025-02-01 14:00:33 +0530

    Add files via upload

---
+++ src/screens/ECommerceWebsite/ECommerceWebsite.jsx
@@ -0,0 +1,260 @@
+import React from "react";
+import "./style.css";
+
+export const ECommerceWebsite = () => {
+  return (
+    <div className="e-commerce-website">
+      <div className="div">
+        <div className="overlap">
+          <div className="part" />
+          <img className="img" alt="Part" src="/img/part-2.svg" />
+          <div className="product-frame">
+            <div className="rectangle" />
+            <div className="rectangle" />
+            <div className="rectangle" />
+            <div className="rectangle" />
+          </div>
+          <div className="product-details">
+            <div className="overlap-group">
+              <img className="ellipse" alt="Ellipse" src="/img/ellipse-9.svg" />
+              <div className="price">Price&nbsp;&nbsp;$25</div>
+              <div className="text-wrapper">boAt Wave Elevate Pro</div>
+              <img className="element" alt="Element" src="/img/81mivd0dakl-sx679-removebg-preview-1.png" />
+            </div>
+            <img className="star" alt="Star" src="/img/star-9.svg" />
+            <div className="text-wrapper-2">5.0</div>
+            <img className="vector" alt="Vector" src="/img/vector-3.png" />
+            <img className="vector-2" alt="Vector" src="/img/vector-2.png" />
+            <img className="vector-3" alt="Vector" src="/img/vector-2.png" />
+            <img className="vector-4" alt="Vector" src="/img/vector-3.png" />
+            <div className="overlap-group-2">
+              <img className="ellipse-2" alt="Ellipse" src="/img/ellipse-10.svg" />
+              <img className="apple-airpods" alt="Apple airpods" src="/img/apple-airpods-1.png" />
+              <div className="text-wrapper-3">Apple Airpods Max</div>
+            </div>
+            <img className="star-2" alt="Star" src="/img/star-9.svg" />
+            <div className="text-wrapper-4">5.0</div>
+            <img className="star-3" alt="Star" src="/img/star-9.svg" />
+            <div className="text-wrapper-5">5.0</div>
+            <img className="star-4" alt="Star" src="/img/star-9.svg" />
+            <div className="text-wrapper-6">5.0</div>
+            <div className="price-2">Price&nbsp;&nbsp;$1299</div>
+            <div className="overlap-2">
+              <img className="ellipse-3" alt="Ellipse" src="/img/ellipse-11.svg" />
+              <div className="text-wrapper-7">ASUS ROG Strix G16</div>
+              <div className="price-3">Price&nbsp;&nbsp;$1676</div>
+              <img className="element-hvxwbpl-sx" alt="Element" src="/img/71h0vxwb3pl-sx679-removebg-preview-1.png" />
+            </div>
+            <div className="overlap-3">
+              <img className="ellipse-4" alt="Ellipse" src="/img/ellipse-12.svg" />
+              <img className="x-controller" alt="X controller" src="/img/x-controller-removebg-preview-1.png" />
+            </div>
+            <div className="overlap-4">
+              <div className="text-wrapper-8">Xbox Gamepad Motion Controller</div>
+              <div className="price-4">Price&nbsp;&nbsp;$1676</div>
+            </div>
+          </div>
+          <img className="blur" alt="Blur" src="/img/blur.svg" />
+          <div className="arrow">
+            <div className="vector-wrapper">
+              <img className="vector-5" alt="Vector" src="/img/vector-4.svg" />
+            </div>
+          </div>
+          <div className="summer-off-banner">
+            <div className="overlap-5">
+              <div className="rectangle-2" />
+              <img className="vector-6" alt="Vector" src="/img/vector-5.svg" />
+              <p className="p">Summer offers from top brands</p>
+              <div className="text-wrapper-9">Buy it now</div>
+              <img className="airpods-removebg" alt="Airpods removebg" src="/img/airpods-removebg-preview-2.png" />
+              <img className="watch-removebg" alt="Watch removebg" src="/img/watch-removebg-preview-1.png" />
+              <img
+                className="apple-macbook-air"
+                alt="Apple macbook air"
+                src="/img/apple-macbook-air-m1-133-wqxga-16gb-256gb-mac-os-us-srebrny-remo.png"
+              />
+              <img className="dog" alt="Dog" src="/img/dog.gif" />
+              <img className="particles" alt="Particles" src="/img/particles-1.gif" />
+            </div>
+          </div>
+          <div className="newest-sellers">
+            <div className="overlap-6">
+              <div className="overlap-group-3">
+                <img
+                  className="professional-mens"
+                  alt="Professional mens"
+                  src="/img/professional-mens-hairstyles-balding-brushed-up-min-1.png"
+                />
+                <img className="happy-young-woman" alt="Happy young woman" src="/img/happy-young-woman-1.png" />
+                <img className="cute-happy-woman" alt="Cute happy woman" src="/img/cute-happy-woman-smiling-1.png" />
+                <img
+                  className="professional-mens-2"
+                  alt="Professional mens"
+                  src="/img/professional-mens-hairstyles-combed-back-min-1.png"
+                />
+              </div>
+              <div className="element-orders-reviews">4,500+ Orders &amp; reviews</div>
+              <div className="text-wrapper-10">Newest Sellers</div>
+              <img className="line" alt="Line" src="/img/line-1.svg" />
+            </div>
+          </div>
+          <div className="popular-brands">
+            <div className="overlap-7">
+              <div className="element-orders-reviews-2">5,200+ Orders &amp; reviews</div>
+              <div className="text-wrapper-11">Popular top 10 brands</div>
+              <img className="line-2" alt="Line" src="/img/line-1.svg" />
+              <div className="brand-logo">
+                <div className="overlap-8">
+                  <img className="samsung-logo" alt="Samsung logo" src="/img/samsung-logo.png" />
+                  <img className="apple-logo" alt="Apple logo" src="/img/apple-logo.png" />
+                  <img className="boat-logo" alt="Boat logo" src="/img/boat-logo.png" />
+                  <div className="asus-logo">
+                    <div className="overlap-group-4">
+                      <div className="ellipse-5" />
+                      <img className="vector-7" alt="Vector" src="/img/vector-6.svg" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <img className="see-all" alt="See all" src="/img/see-all.svg" />
+          <div className="text-wrapper-12">Explore Popular Categories</div>
+          <img className="dailydealsbanner" alt="Dailydealsbanner" src="/img/dailydealsbanner.png" />
+          <div className="main-banner">
+            <div className="overlap-9">
+              <p className="samsung-galaxy">Samsung Galaxy S24 Ultra&nbsp;&nbsp;5G AI Smartphone</p>
+              <div className="overlap-10">
+                <div className="overlap-11">
+                  <div className="overlap-12">
+                    <p className="text-wrapper-13">
+                      Meet Galaxy S24 Ultra, the ultimate form of Galaxy Ultra with a new titanium exterior and a
+                      17.25cm flat display.
+                    </p>
+                    <div className="text-wrapper-14">(2000+ Reviews)</div>
+                  </div>
+                  <div className="rating">
+                    <img className="star-5" alt="Star" src="/img/star-5.svg" />
+                    <img className="star-6" alt="Star" src="/img/star-2.svg" />
+                    <img className="star-7" alt="Star" src="/img/star-5.svg" />
+                    <img className="star-8" alt="Star" src="/img/star-5.svg" />
+                    <img className="star-9" alt="Star" src="/img/star-5.svg" />
+                  </div>
+                </div>
+                <div className="text-wrapper-15">Price $1918</div>
+              </div>
+              <button className="add-to-cart-button">
+                <div className="div-wrapper">
+                  <div className="text-wrapper-16">Add to cart</div>
+                </div>
+              </button>
+              <button className="buy-button">
+                <div className="overlap-13">
+                  <div className="text-wrapper-17">Buy now</div>
+                </div>
+              </button>
+              <div className="quantity">
+                <div className="overlap-14">
+                  <div className="rectangle-3" />
+                  <div className="ellipse-6" />
+                  <div className="ellipse-7" />
+                  <div className="text-wrapper-18">1</div>
+                  <div className="text-wrapper-19">–</div>
+                  <div className="text-wrapper-20">+</div>
+                </div>
+              </div>
+              <div className="overlap-15">
+                <img className="ellipse-8" alt="Ellipse" src="/img/ellipse-3.svg" />
+                <img
+                  className="samsung-galaxy-2"
+                  alt="Samsung galaxy"
+                  src="/img/samsung-galaxy-removebg-preview-2.png"
+                />
+                <img className="particles-2" alt="Particles" src="/img/particles-1.gif" />
+              </div>
+              <div className="favourite-button">
+                <img className="vector-8" alt="Vector" src="/img/vector-7.svg" />
+              </div>
+              <div className="color" />
+              <div className="color-2" />
+              <div className="color-3" />
+              <div className="text-wrapper-21">Color</div>
+            </div>
+          </div>
+          <div className="text-wrapper-22">From top brands</div>
+          <p className="new-trending-gadgets">New &amp; Trending : Gadgets</p>
+        </div>
+        <div className="navbar-background" />
+        <footer className="footer">
+          <div className="overlap-16">
+            <div className="overlap-group-5">
+              <img
+                className="black-and-white"
+                alt="Black and white"
+                src="/img/black-and-white-illustrated-eagle-logo-1.png"
+              />
+              <div className="text-wrapper-23">Digital galaxy</div>
+            </div>
+            <p className="shopkart-com-is-a">
+              shopkart.com is a one stop destination for your family&#39;s fashion needs. We give you the opportunity to
+              give your wardrobe a makeover with the latest collections from our top brands.
+            </p>
+            <div className="text-wrapper-24">MAKE MONEY WITH US</div>
+            <div className="text-wrapper-25">USEFUL LINKS</div>
+            <div className="text-wrapper-26">CONTACT</div>
+            <div className="text-wrapper-27">Sell on ShopKart</div>
+            <div className="text-wrapper-28">Advertise Your Products</div>
+            <div className="text-wrapper-29">Become an Affiliate</div>
+            <div className="text-wrapper-30">Fulfilment by ShopKart</div>
+            <div className="text-wrapper-31">FAQ</div>
+            <div className="text-wrapper-32">Feedback</div>
+            <div className="text-wrapper-33">About Us</div>
+            <div className="text-wrapper-34">Contact Us</div>
+            <div className="text-wrapper-35">digitalgalaxy@gmail.com</div>
+            <img className="email" alt="Email" src="/img/email.svg" />
+            <div className="text-wrapper-36">+91 7904304719</div>
+            <img className="phone" alt="Phone" src="/img/phone.svg" />
+            <p className="element-copyright">
+              <span className="span">© 2024 Copyright digitalgalaxy.in</span>
+            </p>
+            <p className="text-wrapper-37">Get connected with us on social networks:</p>
+            <img className="footer-icons" alt="Footer icons" src="/img/footer-icons.png" />
+          </div>
+        </footer>
+        <img className="cat" alt="Cat" src="/img/cat.gif" />
+        <div className="navbar">
+          <div className="text-wrapper-38">Settings</div>
+          <img className="settings-icon" alt="Settings icon" src="/img/settings-icon.svg" />
+          <div className="text-wrapper-39">Contact us</div>
+          <img className="contact-us-icon" alt="Contact us icon" src="/img/contact-us-icon.png" />
+          <div className="text-wrapper-40">Favourite</div>
+          <img className="favourite" alt="Favourite" src="/img/favourite.svg" />
+          <div className="text-wrapper-41">Cart</div>
+          <img className="cart" alt="Cart" src="/img/cart.svg" />
+          <div className="text-wrapper-42">Explore</div>
+          <img className="explore" alt="Explore" src="/img/explore.svg" />
+          <div className="text-wrapper-43">Home</div>
+          <img className="home-icon" alt="Home icon" src="/img/home-icon.svg" />
+          <div className="section-slide" />
+          <div className="overlap-17">
+            <img className="black-illustrated" alt="Black illustrated" src="/img/black-illustrated-eagle-logo-1.png" />
+            <div className="text-wrapper-44">Digital Galaxy</div>
+          </div>
+        </div>
+        <div className="top">
+          <img className="profile" alt="Profile" src="/img/profile.png" />
+          <img className="notifiy-icon" alt="Notifiy icon" src="/img/notifiy-icon.svg" />
+          <img className="message" alt="Message" src="/img/message.svg" />
+          <div className="search-asset">
+            <div className="overlap-group-6">
+              <img className="vector-9" alt="Vector" src="/img/vector-8.svg" />
+              <div className="text-wrapper-45">Search Product</div>
+              <img className="vector-10" alt="Vector" src="/img/vector-9.svg" />
+              <img className="vector-11" alt="Vector" src="/img/vector-10.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/screens/ECommerceWebsite/index.js
@@ -0,0 +1,1 @@
+export { ECommerceWebsite } from "./ECommerceWebsite";
